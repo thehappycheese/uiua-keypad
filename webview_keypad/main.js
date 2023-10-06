a1652ac368094dc82dc133d48c31b200e98a4069
thekifake <thekidcorner@gmail.com>
2023-10-05 23:49:46 -0400

    replace tooltip-display with tooltip on hover

--- webview_keypad/main.js
+++ webview_keypad/main.js
@@ -7,28 +7,6 @@ try{
     vscode = {};
     vscode.postMessage = (...args)=>console.log("postMessage", ...args);
 }
-let tooltip = document.querySelector("#tooltip-display");
-document.querySelector("#button-holder").addEventListener("mouseover",e=>{
-    if(e.target.attributes["data-title"]){
-        tooltip.innerHTML = e.target.attributes["data-title"].value
-    }
-});
-document.querySelector("#button-holder").addEventListener("mouseup", e=>{
-    if(e.target.attributes["data-title"]){
-        let glyph = e.target.innerHTML;
-        if (glyph=="&lt;"){
-            glyph = "<";
-        }else if(glyph==="&gt;"){
-            glyph=">";
-        }
-        vscode.postMessage({
-            command:"write_glyph",
-            text:glyph,
-            name:e.target.attributes["data-title"].value
-        })
-    }
-})
-
 
 for(let glyph of glyphs){
     let button = document.createElement("button");
